32a8d2a6bbdf0ec8f5caaa6bfbcd8fff658e6968
Tje94 <115041688+Tje94@users.noreply.github.com>
2022-10-24 18:51:23 +0200

    Update main.py
    Code generiek gemaakt na aanleiding van de feedback van Chris

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
-
-# Add your code after this line
-
 player_1 = 'Ruud Gullit'
 player_2 = 'Marco van Basten'
 goal_0 = 32
@@ -13,15 +7,12 @@ scorers = f'{player_1} {goal_0}, {player_2} {goal_1}'
 report = f'{player_1} scored in the {goal_0}nd minute\n{player_2} scored in the {goal_1}th minute'
 
 player = 'Erwin Koeman'
-start_first_name = player.find('E')
-end_first_name = player.find('n')
-first_name = player[:5]
+first_name = player[:player.find(' ')]
 
-start_last_name = player.find('K')
-end_last_name = player.find('n', 6, -1)
-last_name_len = len(player[6:])
+last_name = player[player.find(' ') +1:]
+last_name_len = len(player[player.find(' ') +1:])
 
-name_short = player[0] + '.' + ' ' + player[6:]
+name_short = player[0] + '.' + ' ' + last_name
 
 chant_space = f'{first_name}! ' * len(first_name)
 chant = chant_space.rstrip()
